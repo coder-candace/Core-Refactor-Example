a864e16ece53b55b8916df903f9cae24f03eac8c
Candace Wrather <candaceaw10@gmail.com>
2019-05-30 20:50:48 -0400

    shared projects created

---
+++ Shared.RefactorExample/Shared.RefactorExample.Data/Services/CosmosClientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorExample.Data.Services
+{
+    class CosmosClientService
+    {
+        public static async Task<dynamic> GetAuditLogByIdAsync(string id, string siteId) => "fakeId";
+    }
+}
