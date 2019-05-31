56cdf98f1923b7c94c85f914477bbdc9c7270459
Candace Wrather <candaceaw10@gmail.com>
2019-05-30 22:22:42 -0400

    added core project that references the now netStandard shared projects

---
+++ RefactorExample/RefactorExample.Core.Web/CmsReadOnlyController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RefactorExample.Business;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RefactorExample.Web
+{
+    public class CmsReadOnlyController : Controller
+    {
+        [HttpGet]
+        [Route("getAuditLogById/{id}/{siteId}")]
+        public async Task<HttpResponseMessage> GetAuditLogById(string id, string siteId)
+        {
+            dynamic response = await CosmosConfigBusiness.GetAuditLogByIdAsync(id, siteId);
+            HttpResponseMessage resp = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(response)
+            };
+            return resp;
+        }
+    }
+}
