1b6cf7db1343774ddbd5d40094f95c209bc68d2d
Candace Wrather <candaceaw10@gmail.com>
2019-05-30 20:54:25 -0400

    trying to reference the shared project

--- RefactorExample/RefactorExample.Business/CosmosConfigBusiness.cs
+++ RefactorExample/RefactorExample.Business/CosmosConfigBusiness.cs
@@ -1,18 +1,18 @@
-﻿using RefactorExample.Data.Repositories;
-using System.Threading.Tasks;
+﻿//using RefactorExample.Data.Repositories;
+//using System.Threading.Tasks;
 
-namespace RefactorExample.Business
-{
-    public class CosmosConfigBusiness
-    {
-        public static async Task<dynamic> GetAuditLogByIdAsync(string id, string siteId)
-        {
-            dynamic config = await CosmosRepository.GetAuditLogByIdAsync(id, siteId).ConfigureAwait(false);
-            if (config == null)
-                return null;
+//namespace RefactorExample.Business
+//{
+//    public class CosmosConfigBusiness
+//    {
+//        public static async Task<dynamic> GetAuditLogByIdAsync(string id, string siteId)
+//        {
+//            dynamic config = await CosmosRepository.GetAuditLogByIdAsync(id, siteId).ConfigureAwait(false);
+//            if (config == null)
+//                return null;
 
-            return config;
-        }
+//            return config;
+//        }
 
-    }
-}
+//    }
+//}
